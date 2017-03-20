8e5bbfae3636522159ef4febff339d7c7ecfa0a0
Fernando Badin <fbadin@gmail.com>
2017-03-20 00:55:56 -0400

    created mylibrary about

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -6,22 +6,33 @@ angular.module('libraryApp', ['ui.router','ngResource'])
         $stateProvider
         
             // route for the home page
-            .state('app', {
-                url:'/',
+            .state('app', 
+                {
+                    url:'/',
+                    views: {
+                        'header': {
+                            templateUrl : 'views/header.html',
+                        },
+                        'content': {
+                            templateUrl : 'views/home.html',
+                            controller  : 'IndexController'
+                        },
+                        'footer': {
+                            templateUrl : 'views/footer.html',
+                        }
+                    }
+                })
+
+            // route for the aboutus page
+            .state('app.aboutus', {
+                url:'aboutus',
                 views: {
-                    'header': {
-                        templateUrl : 'views/header.html',
-                    },
-                    'content': {
-                        templateUrl : 'views/home.html',
-                        controller  : 'IndexController'
-                    },
-                    'footer': {
-                        templateUrl : 'views/footer.html',
+                    'content@': {
+                        templateUrl : 'views/aboutus.html',
                     }
                 }
-
-            });
+            })
+            ;
         
         $urlRouterProvider.otherwise('/');
     });
